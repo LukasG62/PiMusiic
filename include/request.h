69e9a559425b4cf2f5f6c4a9ed7ac977aabe875e
LukasG62 <lukas.grando.62@gmail.com>
2024-03-17 22:34:41 +0100

    feat(mpp): Add Doxygen documentation for request handling, implement deletion request creation and sending function
    - Documented request handling functions with Doxygen comments
    - Implemented function to create and send deletion request to the server

--- include/request.h
+++ include/request.h
@@ -1,6 +1,7 @@
 /**
- * \file note.h
- * \brief Définition des fonctions pour envoyer des requetes MPP
+ * @file request.h
+ * @brief Interface contenant les fonctions pour envoyer des requêtes au serveur MPP
+ * @version 1.0
  */
 
 #ifndef REQUEST_H
@@ -9,37 +10,52 @@
 #include "mpp.h"
 
 /**
- * \fn mpp_response_t send_connection_request(char *rfid)
- * \brief Envoie une requete pour obtenir la liste des musiques
- * \return mpp_response_t La réponse du serveur
-*/
-mpp_response_t send_connection_request(socket_t socket, char *rfid);
+ * @fn mpp_response_t send_connection_request(socket_t *socket, char *rfid)
+ * @brief Envoie une requête de connexion
+ * @param socket 
+ * @param rfid 
+ * @return mpp_response_t 
+ */
+mpp_response_t send_connection_request(socket_t *socket, char *rfid);
 
 /**
- * \fn send_list_music_request(char *rfid)
- * \brief Envoie une requete pour obtenir la liste des musiques
- * \return mpp_response_t La réponse du serveur
- * \see mpp_response_t
-*/
-mpp_response_t send_list_music_request(char *rfid);
+ * @fn mpp_response_t send_list_music_request(socket_t *socket, char *rfid)
+ * @brief Envoie une requête pour obtenir la liste des musiques
+ * @param socket La socket de connexion au serveur
+ * @param rfid Le rfid de l'utilisateur
+ * @return mpp_response_t 
+ */
+mpp_response_t send_list_music_request(socket_t *socket, char *rfid);
 
 /**
- * \fn send_save_music_request(char *rfid, music_t *music)
- * \brief Envoie une requete pour sauvegarder une musique
- * \param rfid Le rfid de l'utilisateur
- * \param music La musique à sauvegarder
- * \return mpp_response_t La réponse du serveur
-*/
-mpp_response_t send_save_music_request(char *rfid, music_t *music);
+ * @fn mpp_response_t send_save_music_request(socket_t *socket, char *rfid, music_t *music)
+ * @brief Envoie une requête pour sauvegarder une musique
+ * @param socket La socket de connexion au serveur
+ * @param rfid Le rfid de l'utilisateur
+ * @param music La musique à sauvegarder
+ * @return mpp_response_t 
+ */
+mpp_response_t send_save_music_request(socket_t *socket, char *rfid, music_t *music);
 
 /**
- * \fn send_delete_music_request(char *rfid, music_t *music)
- * \brief Envoie une requete pour supprimer une musique
- * \param rfid Le rfid de l'utilisateur
- * \param music La musique à supprimer
- * \return mpp_response_t La réponse du serveur
-*/
-mpp_response_t send_delete_music_request(char *rfid, time_t musicId);
+ * @fn mpp_response_t send_delete_music_request(socket_t *socket, char *rfid, time_t musicId)
+ * @brief Envoie une requête pour supprimer une musique
+ * @param socket La socket de connexion au serveur
+ * @param rfid Le rfid de l'utilisateur
+ * @param musicId L'id musique à supprimer
+ * @return mpp_response_t 
+ */
 
+mpp_response_t send_delete_music_request(socket_t *socket, char *rfid, time_t musicId);
+/**
+ * @fn mpp_response_t send_get_music_request(socket_t *socket, char *rfid, time_t musicId)
+ * @brief Envoie une requête pour récupérer une musique
+ * @param socket La socket de connexion au serveur
+ * @param rfid Le rfid de l'utilisateur
+ * @param musicId L'identifiant de la musique
+ * @return mpp_response_t 
+ * @see mpp_response_t
+ */
+mpp_response_t send_get_music_request(socket_t *socket, char *rfid, time_t musicId);
 
 #endif
--- src/request.c
+++ src/request.c
@@ -1,11 +1,18 @@
+/**
+ * @file request.c
+ * @brief Requêtes pour le serveur MPP
+ * @version 1.0
+ */
 #include "request.h"
 
 /**
- * \fn mpp_response_t send_connection_request(char *rfid)
- * \brief Envoie une requete pour obtenir la liste des musiques
- * \return mpp_response_t La réponse du serveur
-*/
-mpp_response_t send_connection_request(socket_t socket, char *rfid) {
+ * @fn mpp_response_t send_connection_request(socket_t *socket, char *rfid)
+ * @brief Envoie une requête de connexion
+ * @param socket 
+ * @param rfid 
+ * @return mpp_response_t 
+ */
+mpp_response_t send_connection_request(socket_t *socket, char *rfid) {
     mpp_response_t response;
     mpp_request_t request;
 
@@ -13,40 +20,81 @@ mpp_response_t send_connection_request(socket_t socket, char *rfid) {
     request = create_mpp_request(MPP_CONNECT, rfid, NULL, NO_MUSIC_ID);
 
     // On envoie la requête
-    send_data(&socket, &request, (serialize_t) serialize_mpp_request);
+    send_data(socket, &request, (serialize_t) serialize_mpp_request);
     // On attend la réponse
-    recv_data(&socket, &response, (serialize_t) deserialize_mpp_response);
+    recv_data(socket, &response, (serialize_t) deserialize_mpp_response);
 
     return response;
 }
 
 /**
- * \fn send_list_music_request(char *rfid)
- * \brief Envoie une requete pour obtenir la liste des musiques
- * \return mpp_response_t La réponse du serveur
- * \see mpp_response_t
-*/
-mpp_response_t send_list_music_request(char *rfid) {
-    mpp_response_t response;
-    mpp_request_t request;
-
+ * @fn mpp_response_t send_list_music_request(socket_t *socket, char *rfid)
+ * @brief Envoie une requête pour obtenir la liste des musiques
+ * @param socket La socket de connexion au serveur
+ * @param rfid Le rfid de l'utilisateur
+ * @return mpp_response_t 
+ */
+mpp_response_t send_list_music_request(socket_t *socket, char *rfid) {
+    mpp_response_t response = create_mpp_response(MPP_RESPONSE_BAD_REQUEST, "", NULL, NULL);
+    mpp_request_t request = create_mpp_request(MPP_LIST_MUSIC, rfid, NULL, NO_MUSIC_ID);
     // On envoie la requête
+    send_data(socket, &request, (serialize_t) serialize_mpp_request);
+    // On attend la réponse
+    recv_data(socket, &response, (serialize_t) deserialize_mpp_response);
+    return response;
 }
 
 /**
- * \fn send_save_music_request(char *rfid, music_t *music)
- * \brief Envoie une requete pour sauvegarder une musique
- * \param rfid Le rfid de l'utilisateur
- * \param music La musique à sauvegarder
- * \return mpp_response_t La réponse du serveur
-*/
-mpp_response_t send_save_music_request(char *rfid, music_t *music);
+ * @fn mpp_response_t send_save_music_request(socket_t *socket, char *rfid, music_t *music)
+ * @brief Envoie une requête pour sauvegarder une musique
+ * @param socket La socket de connexion au serveur
+ * @param rfid Le rfid de l'utilisateur
+ * @param music La musique à sauvegarder
+ * @return mpp_response_t 
+ */
+mpp_response_t send_save_music_request(socket_t *socket, char *rfid, music_t *music) {
+    mpp_response_t response;
+    mpp_request_t request = create_mpp_request(MPP_ADD_MUSIC, rfid, music, NO_MUSIC_ID);
+    
+    send_data(socket, &request, (serialize_t) serialize_mpp_request);
+    recv_data(socket, &response, (serialize_t) deserialize_mpp_response);
+
+    return response;
+}
 
 /**
- * \fn send_delete_music_request(char *rfid, music_t *music)
- * \brief Envoie une requete pour supprimer une musique
- * \param rfid Le rfid de l'utilisateur
- * \param music La musique à supprimer
- * \return mpp_response_t La réponse du serveur
-*/
-mpp_response_t send_delete_music_request(char *rfid, time_t musicId);
+ * @fn mpp_response_t send_delete_music_request(socket_t *socket, char *rfid, time_t musicId)
+ * @brief Envoie une requête pour supprimer une musique
+ * @param socket La socket de connexion au serveur
+ * @param rfid Le rfid de l'utilisateur
+ * @param musicId L'id musique à supprimer
+ * @return mpp_response_t 
+ */
+mpp_response_t send_delete_music_request(socket_t *socket, char *rfid, time_t musicId) {
+    mpp_response_t response;
+    mpp_request_t request = create_mpp_request(MPP_DELETE_MUSIC, rfid, NULL, musicId);
+    
+    send_data(socket, &request, (serialize_t) serialize_mpp_request);
+    recv_data(socket, &response, (serialize_t) deserialize_mpp_response);
+
+    return response;
+}
+
+/**
+ * @fn mpp_response_t send_get_music_request(socket_t *socket, char *rfid, time_t musicId)
+ * @brief Envoie une requête pour récupérer une musique
+ * @param socket La socket de connexion au serveur
+ * @param rfid Le rfid de l'utilisateur
+ * @param musicId L'identifiant de la musique
+ * @return mpp_response_t 
+ * @see mpp_response_t
+ */
+mpp_response_t send_get_music_request(socket_t *socket, char *rfid, time_t musicId) {
+    mpp_response_t response;
+    mpp_request_t request = create_mpp_request(MPP_GET_MUSIC, rfid, NULL, musicId);
+    
+    send_data(socket, &request, (serialize_t) serialize_mpp_request);
+    recv_data(socket, &response, (serialize_t) deserialize_mpp_response);
+
+    return response;
+}
